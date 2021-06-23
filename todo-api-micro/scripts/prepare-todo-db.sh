7db6ae3d54c0b1c177baa05656f8b82104bbb88a
roxcarpio <rocarlo91@gmail.com>
2021-06-23 15:20:07 +0200

    Update prepare-todo-db.sh

--- todo-api-micro/scripts/prepare-todo-db.sh
+++ todo-api-micro/scripts/prepare-todo-db.sh
@@ -5,10 +5,6 @@ oc new-project database
 wget https://raw.githubusercontent.com/roxcarpio/do-288-code/main/todo-api-micro/scripts/db.yaml -O $YAML_FILE
 
 oc create -f $YAML_FILE
-
-<<<<<<< HEAD
-rm -rf YAML_FILE
-=======
 rm -rf YAML_FILE
 
 FLAG_PROYECT=$(oc projects | grep unit9)
@@ -19,4 +15,3 @@ then
 else  
   oc project unit9
 fi
->>>>>>> 66d4a1a87d2b54c1f8be7c4fdc29b0b08c585cc7
